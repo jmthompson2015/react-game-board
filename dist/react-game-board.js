f867411e27adbab02fb44e9609cee1c635b6122e
jmthompson2015 <softwaredev@jeffreythompson.net>
2019-09-26 20:04:43 -0600

    Fixed images prop type.

--- dist/react-game-board.js
+++ dist/react-game-board.js
@@ -657,7 +657,7 @@
     gridColor: PropTypes.string,
     gridLineWidth: PropTypes.number,
     height: PropTypes.number,
-    images: PropTypes.arrayOf(),
+    images: PropTypes.arrayOf(PropTypes.string),
     isCellUsedFunction: PropTypes.func,
     myKey: PropTypes.string,
     onClick: PropTypes.func,
